e999e93b11b8db815c536aef8328f54efc830e05
Aleksy Grabowski <hurufu@gmail.com>
2024-09-08 12:43:06 +0200

    Add more goal expantion tests

--- src/tests/goal_expansion/main.pl
+++ src/tests/goal_expansion/main.pl
@@ -7,44 +7,44 @@
 :- use_module('../test_framework').
 :- use_module(m).
 
+% Support predicates
+p(X) :- X #= 0.
+q.
+same(A,A).
 clpz:monotonic.
 
+% https://github.com/mthom/scryer-prolog/issues/2361
 test("#2361 1", (
     gs([length("a",L1),length("ab", L2)]),
     (L1,L2) == (1,2)
 )).
-
 test("#2361 2", (
     gs([(length("a",L1),length("ab", L2))]),
     (L1,L2) == (1,2)
 )).
-
 test("#2361 3", (
     findall(X, gs([(append("abc","cd",X);length("abcd",X))]), ["abccd", 4])
 )).
-
 test("#2361 4", (
     gs([(append("abc","cd",Ls)->length("abcd", L))]),
     (Ls,L) == ("abccd",4)
 )).
-
 test("#2236 1", (
     catch(_ #= 0, E, true),
     nonvar(E),
     E = error(instantiation_error,_)
 )).
-
 test("#2236 2", (
     catch(p(_), E, true),
     nonvar(E),
     E = error(instantiation_error,_)
 )).
 
+% https://github.com/mthom/scryer-prolog/issues/2255
 test("#2255 1", (G = (_+\(user:q)), G)).
 test("#2255 2", (G = (_+\q), G)).
 test("#2255 3", (call(_+\q))).
 test("#2255 4", (X = (_+\q), same(X, Y), Y)).
-
 test("#2255 5", (
     setup_call_cleanup(
         asserta((t2:-X=(_+\q),X)),
@@ -52,7 +52,6 @@ test("#2255 5", (
         retractall(t2)
     )
 )).
-
 test("#2255 6", (
     setup_call_cleanup(
         asserta((t3:-call(_+\q))),
@@ -61,13 +60,42 @@ test("#2255 6", (
     )
 )).
 
+% https://github.com/mthom/scryer-prolog/issues/2202
 test("#2202 1", (
     catch(call(1,_), E, true),
     nonvar(E),
     E = error(type_error(callable,1),_)
 )).
 
+% https://github.com/mthom/scryer-prolog/issues/2062
+test("#2062 1", (
+    X = 2, maplist(#\=(X), [0,1]).
+)).
+test("#2062 2", (
+    maplist(#\=(X), [0,1])
+)).
+test("#2062 3", (
+    maplist(call(#\=,X), [0,1])
+)).
+test("#2061 4", (
+    maplist(call(#\=(X)), [0,1])
+)).
 
-p(X) :- X #= 0.
-q.
-same(A,A).
+% https://github.com/mthom/scryer-prolog/issues/1977
+test("#1977 1", (
+    call_with_inference_limit(true, 1, R),
+    R == inference_limit_exceeded
+)).
+test("#1977 2", (
+    call_with_inference_limit(true, 2, R),
+    R == !
+)).
+test("#1977 3", (
+    length(_, L),
+    call_with_inference_limit(true, L, !)
+))
+
+% https://github.com/mthom/scryer-prolog/issues/1987
+test("#1987 1", (
+    call_with_inference_limit(true,1,!)
+)).
