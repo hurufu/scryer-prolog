0d012ecd52532913bcd821eaf0b525e331a7c01e
Aleksy Grabowski <hurufu@gmail.com>
2024-09-11 09:22:09 +0200

    Fix typos and indentation

--- benches/call_expansion.pl
+++ benches/call_expansion.pl
@@ -4,18 +4,18 @@
 :- use_module(library(lists)).
 
 test(2^N, Version) :-
-	length(_,N),
-	N > 17,
-	BN is 2^N,
-	length(L, BN),
-	time(t(Version, L)).
+    length(_,N),
+    N > 17,
+    BN is 2^N,
+    length(L, BN),
+    time(t(Version, L)).
 
 t(call,L) :-
-	call(unify_with_occurs_check(L),V),
-	V \= [].
+   call(unify_with_occurs_check(L),V),
+   V \= [].
 t(direct,L) :-
-	unify_with_occurs_check(L,V),
-	V \= [].
+   unify_with_occurs_check(L,V),
+   V \= [].
 t(icall,L) :-
    G_1 = unify_with_occurs_check(L),
    call(G_1, V),
--- src/tests/dcgs.pl
+++ src/tests/dcgs.pl
@@ -3,35 +3,35 @@
 :- use_module(library(dcgs)).
 :- use_module(test_framework).
 
-test("cuts inside DCG do not blead to outer scope #1", (
+test("cuts inside DCG do not bleed to outer scope #1", (
     findall(L, (phrase(foo, [_]); L=1), [1])
 )).
 
-test("cuts inside DCG do not blead to outer scope #2", (
+test("cuts inside DCG do not bleed to outer scope #2", (
     findall(L, (phrase(!, [_]); L=1), [1])
 )).
 
-test("cuts inside DCG do not blead to outer scope #3", (
+test("cuts inside DCG do not bleed to outer scope #3", (
     findall(L, (phrase(("a",!), [_]); L=1), [1])
 )).
 
-test("cuts inside DCG do not blead to outer scope #4", (
+test("cuts inside DCG do not bleed to outer scope #4", (
     findall(L, (phrase(("a";!), [_]); L=1), [1])
 )).
 
-test("cuts inside DCG do not blead to outer scope #5", (
+test("cuts inside DCG do not bleed to outer scope #5", (
     findall(L, (phrase(bar:','(!,!), [_]); L=1), [1])
 )).
 
-test("cuts inside DCG do not blead to outer scope #6", (
+test("cuts inside DCG do not bleed to outer scope #6", (
     findall(L, (phrase(bar:!, [_]); L=1), [1])
 )).
 
-test("cuts inside DCG do not blead to outer scope #7", (
+test("cuts inside DCG do not bleed to outer scope #7", (
     C=!, findall(L, (phrase(C, [_]); L=1), [1])
 )).
 
-test("cuts inside DCG do not blead to outer scope #8", (
+test("cuts inside DCG do not bleed to outer scope #8", (
     findall(L, (phrase({!}, [_]); L=1), [1])
 )).
 
